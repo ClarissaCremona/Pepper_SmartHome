616d0b8b1080c9f27984c48b752125724b97d59b
Clarissa <cremona.clarissa@gmail.com>
2017-06-09 15:50:33 +0100

    raksjdf

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/clarissa/catkin_ws/src
+# Build directory: /home/clarissa/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(beginner_tutorials)
+SUBDIRS(my_pkg)
+SUBDIRS(naoqi_driver)
---
+++ catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/DependInfo.cmake
@@ -0,0 +1,69 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/converters/audio.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/audio.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/converters/camera.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/camera.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/converters/diagnostics.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/diagnostics.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/converters/imu.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/imu.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/converters/info.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/info.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/converters/joint_state.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/joint_state.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/converters/laser.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/laser.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/converters/log.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/log.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/converters/memory/bool.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/memory/bool.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/converters/memory/float.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/memory/float.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/converters/memory/int.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/memory/int.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/converters/memory/string.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/memory/string.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/converters/memory_list.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/memory_list.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/converters/odom.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/odom.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/converters/sonar.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/sonar.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/converters/touch.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/touch.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/event/audio.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/event/audio.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/event/touch.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/event/touch.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/helpers/driver_helpers.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/helpers/driver_helpers.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/naoqi_driver.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/naoqi_driver.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/publishers/camera.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/publishers/camera.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/publishers/info.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/publishers/info.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/publishers/joint_state.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/publishers/joint_state.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/publishers/log.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/publishers/log.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/publishers/sonar.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/publishers/sonar.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/recorder/camera.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/recorder/camera.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/recorder/diagnostics.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/recorder/diagnostics.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/recorder/globalrecorder.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/recorder/globalrecorder.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/recorder/joint_state.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/recorder/joint_state.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/recorder/log.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/recorder/log.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/recorder/sonar.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/recorder/sonar.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/services/robot_config.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/services/robot_config.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/subscribers/moveto.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/subscribers/moveto.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/subscribers/speech.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/subscribers/speech.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/subscribers/teleop.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/subscribers/teleop.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/tools/from_any_value.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/tools/from_any_value.cpp.o"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/src/tools/robot_description.cpp" "/home/clarissa/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/tools/robot_description.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "CATKIN_BUILD"
+  "LIBQI_VERSION=23"
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"naoqi_driver\""
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "/home/clarissa/catkin_ws/src/naoqi_driver/include"
+  "/opt/ros/indigo/include"
+  "/usr/include/eigen3"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
